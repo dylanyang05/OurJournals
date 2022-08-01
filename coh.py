693c4d8738d7b4b086dcf13f07a23faa53de884e
Natasha <S10247541@connect.np.edu.sg>
2022-08-01 17:00:05 +0800

    Amendments

--- coh.py
+++ coh.py
@@ -5,22 +5,18 @@ import re
 
 
 print("hello")
-print("I have no idea")
 def read_csv(output_path,lines):
     with open(output_path,"w", newline="") as f:
         reader = csv.reader
 def coh ():
     path = path.cwd()/"OURJOURNALS"/"cash_on_hand.csv"
-    home = path.home()
-    file_path = home/"OURJOURNALS"
-    cash_on_hand = path.read("*.txt")
-    print(cash_on_hand)
 
     lines = []
-    for cash in cash_on_hand:
-        with open(cash) as f:
-            text = f.read()
-            lines.append(search(text))
+    for cash in path:
+        with cash.open(mode='r',encoding='UTF-8') as f:
+            coh = {}
+            
 
-        read_csv(path/"cash_on_hand.csv",lines) 
+
+        path.csv.read(),lines
 
