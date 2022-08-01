a43903aea22e85f5b20646a0ab6976c9af1c200e
Natasha <S10247541@connect.np.edu.sg>
2022-08-01 13:54:10 +0800

    i have no idea

--- coh.py
+++ coh.py
@@ -1,1 +1,8 @@
 print("hello")
+print("I have no idea")
+def main ():
+    path = path.cwd()/"OURJOURNALS"/"cash_on_hand.csv"
+    home = path.home()
+    file_path = home/"OURJOURNALS"
+    cash_on_hand = path.glob("*.txt")
+    print(cash_on_hand)
