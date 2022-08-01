7f822bc428b84171476ee25709817489da035446
Natasha <S10247541@connect.np.edu.sg>
2022-08-01 15:29:11 +0800

    idk

--- coh.py
+++ coh.py
@@ -1,8 +1,24 @@
+from asyncore import write
+import csv
+from re import search
+
+
 print("hello")
 print("I have no idea")
-def main ():
+def read_csv(output_path,lines):
+    with open(output_path,"w", newline="") as f:
+        reader = csv.reader
+def coh ():
     path = path.cwd()/"OURJOURNALS"/"cash_on_hand.csv"
     home = path.home()
     file_path = home/"OURJOURNALS"
     cash_on_hand = path.glob("*.txt")
     print(cash_on_hand)
+
+    lines = []
+    for cash in cash_on_hand:
+        with open(cash) as f:
+            text = f.read()
+            lines.append(search(text))
+
+        read_csv(path/"cash_on_hand.csv",lines) 
