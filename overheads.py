ce7c366848df9927f394edbcecc5435fea1affe2
JingEn <s10247472@connect.np.edu.sg>
2022-08-01 17:21:45 +0800

    update

--- overheads.py
+++ overheads.py
@@ -1,1 +1,10 @@
-print("hello")
+import csv
+import re
+
+def read_csv(output_path,lines):
+    with open (output_path, "w", newline="" ) as file:
+        reader= csv.reader
+
+def oh ():
+    path = path.cwd()/"OURJOURNALS"/"Overheads.csv"
+    
