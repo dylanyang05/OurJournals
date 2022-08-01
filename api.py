a03fb8e9b755d197696b4ae6a8ef163ef824ba30
Dylan Yang <s10239872@connect.np.edu.sg>
2022-08-01 14:18:32 +0800

    api changes

--- api.py
+++ api.py
@@ -1,11 +1,14 @@
 import requests
 
-key = "L9FUWF60MFPOYCI3"
-url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={key}"
-response = requests.get(url)
+def convert(sgd):
+    key = "L9FUWF60MFPOYCI3"
+    url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={key}"
+    response = requests.get(url)
 
-forex = response.json()
-print(forex)
+    forex = response.json()
 
-rate = forex['Realtime Currency Exchange Rate']['5. Exchange Rate']
-print(rate)
+    rate = forex['Realtime Currency Exchange Rate']['5. Exchange Rate']
+    USD = float(sgd) * float(rate)
+    return round(USD,2)
+
+print(convert(100))
