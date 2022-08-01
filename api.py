a9110cf77ae413af15d56c9767cbd591cf61fbc9
Dylan Yang <s10239872@connect.np.edu.sg>
2022-08-01 14:44:41 +0800

    stuff

--- api.py
+++ api.py
@@ -1,14 +1,11 @@
 import requests
 
+key = "L9FUWF60MFPOYCI3"
+url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={key}"
+response = requests.get(url)
+forex = response.json()
+rate = forex['Realtime Currency Exchange Rate']['5. Exchange Rate']
+
 def convert(sgd):
-    key = "L9FUWF60MFPOYCI3"
-    url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={key}"
-    response = requests.get(url)
-
-    forex = response.json()
-
-    rate = forex['Realtime Currency Exchange Rate']['5. Exchange Rate']
     USD = float(sgd) * float(rate)
     return round(USD,2)
-
-print(convert(100))
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello")
+import api, cash_on_hands, overheads, profit_loss
+
+def main():
+    forex = api.convert()
+    overheads.overhead_function(forex)
+    cash_on_hands.coh_function(forex)
+    profit_loss.pl_function(forex)
+
+main()
+
+print(f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = {rate}")
+print(f"[HIGHEST OVERHEADS] {} = {}")
+print(f"[CASH DEFICIT] DAY: {}, AMOUNT: {}")
+print(f"[PROFIT DEFICIT] DAY: {}, AMOUNT: {}")
