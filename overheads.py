461d76c2cfc4b90fa80d2fa32c1d184d5c132835
JingEn <s10247472@connect.np.edu.sg>
2022-08-03 22:47:28 +0800

    hi

--- overheads.py
+++ overheads.py
@@ -3,36 +3,36 @@ from pathlib import Path
 from unicodedata import category
 
 #Create file path to overheads csv file
-Overheads_file= Path.cwd()/"csv_reports"/"Overheads.csv"
+Overheads= Path.cwd()/"csv_reports"/"Overheads.csv"
 
-#Create an empty list to store data into the list 
-OurJournals_Overheads_list=[]
-final_Overheadslist= []
+#Create an 2 empty list to store data into the list 
+Overheads_list=[]
+Overheads_amount= []
 
 #Using the .open() funtion to acess mode of file which is for reading the csv file, encoding which is to inclue longer/all characters and save files as overheads
-with Overheads_file.open (mode="r", encoding= "UTF-8") as Overheads:
+with Overheads.open (mode="r", encoding= "UTF-8") as file:
     
 #Using .reader() function in csv module to read the data in overheads file
-    readfiles= csv.reader(Overheads)
+    readfiles= csv.reader(file)
 
 #Using next() function to return the next item in the list
     next(readfiles)
 
 #Apply for loop to create a list comprehension expression since there is 2 items in each tuple, largest value and list that consist of different expenses
     for line in readfiles:
-        OurJournals_Overheads_list.append(line)
-        final_Overheadslist.append(float(line[1]))
 
-print(OurJournals_Overheads_list)        
-print(final_Overheadslist)
+#Using the append function to add read files into the list
+        Overheads_list.append(line)
 
-highestvalue=max(final_Overheadslist)
-for information in OurJournals_Overheads_list:
+#A
+        Overheads_amount.append(float(line[1]))
+
+highestvalue=max(Overheads_amount)
+for information in Overheads_list:
     if information[1] == str(highestvalue):
         highest_overhead=information[0]
 
-def highestOverheads_details():
-    message= (f"[HIGHEST OVERHEADS]: {highest_overhead}, AMOUNT: SGD{highestvalue}")
+def Overheads_details():
+    message= (f"[HIGHEST OVERHEADS]: {highest_overhead}: SGD{highestvalue}")
     print (message)
-highestOverheads_details()
-print("hi")
+Overheads_details()
