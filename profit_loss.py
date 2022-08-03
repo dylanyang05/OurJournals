7f68b8fa26c7add6a4d28510d4806b5a9101c328
mandyymc02 <s10243029@connect.np.edu.sg>
2022-08-04 01:58:01 +0800

    profit and loss change

--- profit_loss.py
+++ profit_loss.py
@@ -5,25 +5,26 @@ from pathlib import Path
 profit_loss_file = Path.cwd()/"csv_reports"/"Profits and Loss.csv"
 
 profit_loss_list = []
-final_pnl_list = []
+pnl_amount = []
 
 with profit_loss_file.open(mode = "r", encoding = "UTF-8", newline = "") as profit_loss :
     readfiles = csv.reader(profit_loss)
     next(readfiles)
     for line in readfiles: 
         profit_loss_list.append(line)
+        pnl_amount.append(float(line[4]))
 
-#print(profit_loss_list)
+summary_path = Path.cwd()/"summary_reports.txt"
 
-el= []
-el2= []
-for numbers in profit_loss_list:
-   day = numbers[0]
-   amount = numbers[4]
-   el.append(day)
-   el2.append(amount)
-   #print(el)
-   #print(el2)
-   print(slice(amount))
+def pnl_difference(rate):
+    for number in range(0,(len(pnl_amount)-1)):
+        first_day = pnl_amount[number]
+        second_day = pnl_amount[number+1]
+        if first_day > second_day:
+            difference = first_day - second_day
+            difference_sgd = difference * rate
+            difference_day = profit_loss_list[number+1][0]
+            with summary_path.open (mode= 'a', encoding= 'UTF-8') as file:
+                file.write(f"[CASH DEFICIT] DAY{difference_day}, AMOUNT : SGD{difference_sgd}")
 
 
