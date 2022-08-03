c2e2d7ba4904adcfcff94eb400053ef171bbbb27
Serene1702 <s10247193@connect.np.edu.sg>
2022-08-03 17:23:44 +0800

    h

--- overheads.py
+++ overheads.py
@@ -9,15 +9,15 @@ def read_csv(output_path,lines):
 def oh ():
     path = path.cwd()/"OURJOURNALS"/"Overheads.csv"
 
-overhead_info = [['Salary Expense',30.88], 
-                ['Internet Expense',0.47], 
-                ['Marketing Expense',4.93],
-                ['Rental Expense',20.06],
-                ['Overflow Expense-Retail',0.84],
+overhead_info = [['Salary Expense', 30.88], 
+                ['Internet Expense', 0.47], 
+                ['Marketing Expense', 4.93],
+                ['Rental Expense', 20.06],
+                ['Overflow Expense-Retail', 0.84],
                 ['Penalty Expense', 8.91],
-                ['Depreciation Expense',16.85],
-                ['Shipping Expense',0.54],
+                ['Depreciation Expense', 16.85],
+                ['Shipping Expense', 0.54],
                 ['Human Resource Expense', 16.52]]
-higheest_expense = overhead_info.values()
-max_expense= max(higheest_expense)
+highest_expense = overhead_info.values()
+max_expense = max(highest_expense)
 print ("[HIGHEST OVERHEAD] SALARY EXPENSE:"), max_expense
