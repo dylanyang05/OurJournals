55d2f3be51faeee10fb2ea3d72b2741909492f21
Serene1702 <s10247193@connect.np.edu.sg>
2022-08-03 21:38:18 +0800

    h

--- profit_loss.py
+++ profit_loss.py
@@ -10,27 +10,41 @@ with profit_loss_file.open(mode = "r", encoding = "UTF-8", newline = "") as prof
     readfiles = csv.reader(profit_loss)
     next(readfiles)
     for line in readfiles: 
-        profit_loss_list.append(line[1])
+        profit_loss_list.append(line[4])
+
+def info() :
+    info = [["Day40", "5298840"], ["Day41", "5197222"], ["Day42", "5319067"], ["Day43", "5374926"], ["Day44", "5866565"], ["Day45", "5884455"]]
 
 info_profitloss_list = []
-for information in profit_loss_list :
-    information = float(information)
-    info_profitloss_list.append(information)
+for info in profit_loss_list :
+    info = float(info)
+    info_profitloss_list.append(info)
 
-print(info_profitloss_list)
+day_40 = float(info[0][1])
+day_41 = float(info[1][1])
+day_42 = float(info[2][1])
+day_43 = float(info[3][1])
+day_44 = float(info[4][1])
+day_45 = float(info[5][1])
 
-profit_loss = [["Day40", "5298840"], ["Day41", "5197222"], ["Day42", "5319067"], ["Day43", "5374926"], ["Day44", "5866565"], ["Day45", "5884455"]]
-day_40 = float(profit_loss[0][1])
-day_41 = float(profit_loss[1][1])
-day_42 = float(profit_loss[2][1])
-day_43 = float(profit_loss[3][1])
-day_44 = float(profit_loss[4][1])
-day_45 = float(profit_loss[5][1])
-
-difference1 = day_41 - day_40
+difference1 = day_41 - day_40 
 difference2 = day_43 - day_42
 difference3 = day_45 - day_44
 
 print("The profit and loss on Day 41 is lower than Day 40 by", abs(difference1))
 print("The profit and loss on Day 43 is lower than Day 42 by", (difference2))
 print("The profit and loss on Day 45 is lower than Day 44 by", (difference3))
+
+#print(info_profitloss_list)
+
+
+#day_40 = float(profit_loss[0][1])
+#day_41 = float(profit_loss[1][1])
+#day_42 = float(profit_loss[2][1])
+#day_43 = float(profit_loss[3][1])
+#day_44 = float(profit_loss[4][1])
+#day_45 = float(profit_loss[5][1])
+
+#difference1 = day_41 - day_40
+#difference2 = day_43 - day_42
+#difference3 = day_45 - day_44
