c75065a4a4ad8a462a7677c0ca7b5c0f7922cffe
JingEn <s10247472@connect.np.edu.sg>
2022-08-03 17:22:49 +0800

    updated 522pm

--- overheads.py
+++ overheads.py
@@ -2,27 +2,27 @@ import csv
 from pathlib import Path
 
 
-filepath= Path.cwd()/"OurJournals"
+filepath= Path.cwd()/"Overheads.csv"
 
-Overheads_file= Path.cwd()/"OurJournals"/"Overheads.csv"
+Overheads_file= Path.cwd()/"Overheads.csv"
 
 OurJournals_Overheads_list=[]
 
-with Overheads_file.open (mode="r", encoding= "UTF-8") as Overheads:
+with Overheads_file.open (mode="r", encoding= "UTF-8", newline='') as Overheads:
     readfiles= csv.reader(Overheads)
     for line in readfiles:
         OurJournals_Overheads_list.append(line[1])
         
 final_Overheadslist= []
-for details in OurJournals_Overheads_list:
-    details=float (details)
-    final_Overheadslist.append(details)
+for information in OurJournals_Overheads_list:
+    information=float (information)
+    final_Overheadslist.append(information)
 
 
 def highestOverheads_details():
-   
-     for overheaddetails in Overheads_file:
-        highestvalue= max(Overheads_file)
+    highestvalue= max(final_Overheadslist)
+    for overheaddetails in final_Overheadslist:
+        highestvalue= max(final_Overheadslist)
         if overheaddetails > highestvalue:
             highestvalue=overheaddetails
         message= (f"[HIGHEST VALUE] Salary Expense:", highestvalue)
