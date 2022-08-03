113b61d2ff319ac75c30f6856ae7fb5d7e5ef3e9
JingEn <s10247472@connect.np.edu.sg>
2022-08-03 18:19:55 +0800

    updated619pm

--- overheads.py
+++ overheads.py
@@ -3,20 +3,22 @@ from pathlib import Path
 from unicodedata import category
 
 
-Overheads_file= Path.cwd()/"OurJournals"/"Overheads.csv"
+Overheads_file= Path.cwd()/"OURJOURNALS"/"Overheads.csv"
 
 OurJournals_Overheads_list=[]
 
 with Overheads_file.open (mode="r", encoding= "UTF-8", newline='') as Overheads:
     readfiles= csv.reader(Overheads)
+    next(readfiles)
     for line in readfiles:
         OurJournals_Overheads_list.append(line[1])
         
 final_Overheadslist= []
 for information in OurJournals_Overheads_list:
-    information=float (information)
+    information= float(information)
     final_Overheadslist.append(information)
 
+print(final_Overheadslist)
 
 def highestOverheads_details():
     highestvalue=-1
@@ -24,5 +26,5 @@ def highestOverheads_details():
         if overheaddetails> highestvalue:
             highestvalue=overheaddetails
             index=(final_Overheadslist.index(highestvalue))
-            message= (f"[HIGHEST OVERHEADS]", category[index],":", highestvalue)
+            message= (f"[HIGHEST OVERHEADS]",category[index],":", highestvalue)
     print (message)
