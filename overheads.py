a307193fec29ddf846010e101538257c973ccf9c
Dylan Yang <s10239872@connect.np.edu.sg>
2022-08-03 22:27:50 +0800

    sd

--- overheads.py
+++ overheads.py
@@ -6,26 +6,24 @@ from unicodedata import category
 Overheads_file= Path.cwd()/"csv_reports"/"Overheads.csv"
 
 OurJournals_Overheads_list=[]
+final_Overheadslist= []
 
 with Overheads_file.open (mode="r", encoding= "UTF-8", newline='') as Overheads:
     readfiles= csv.reader(Overheads)
     next(readfiles)
     for line in readfiles:
-        OurJournals_Overheads_list.append(line[1])
-        
-final_Overheadslist= []
-for information in OurJournals_Overheads_list:
-    information= float(information)
-    final_Overheadslist.append(information)
+        OurJournals_Overheads_list.append(line)
+        final_Overheadslist.append(float(line[1]))
 
+print(OurJournals_Overheads_list)        
 print(final_Overheadslist)
 
-def highestOverheads_details():
+highestvalue=max(final_Overheadslist)
+for information in OurJournals_Overheads_list:
+    if information[1] == str(highestvalue):
+        highest_overhead=information[0]
 
-    #for overheaddetails in final_Overheadslist:
-        #if overheaddetails> highestvalue:
-            highestvalue=max(final_Overheadslist)
-            #index=(final_Overheadslist.index(highestvalue))
-            message= (f"[HIGHEST OVERHEADS]:", highestvalue)
-            print (message)
+def highestOverheads_details():
+    message= (f"[HIGHEST OVERHEADS]: {highest_overhead}, AMOUNT: SGD{highestvalue}")
+    print (message)
 highestOverheads_details()
